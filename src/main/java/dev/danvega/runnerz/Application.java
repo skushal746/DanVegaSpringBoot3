45d160e6219958cab1a38b7c73a082bcc95b67c6
Kushal Sharma <kushalsharma@Kushals-MacBook-Air.local>
2024-08-28 06:48:14 -0400

    Initial commit

---
+++ src/main/java/dev/danvega/runnerz/Application.java
@@ -0,0 +1,17 @@
+package dev.danvega.runnerz;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	private static final Logger log = LoggerFactory.getLogger(Application.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+		log.info("Application started successfully");
+	}
+}
---
+++ src/main/java/dev/danvega/runnerz/run/Run.java
@@ -0,0 +1,17 @@
+package dev.danvega.runnerz.run;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+
+public class Run {
+
+    private Integer id;
+    private String title;
+    private LocalDateTime startedOn;
+    private LocalDateTime completedOn;
+    private Integer miles;
+    private Location location;
+
+
+
+}
